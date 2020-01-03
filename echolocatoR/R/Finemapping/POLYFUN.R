db18476238d1ad4ccb49500a21b316b3520e9156
Brian Schilder <schilb03@lg03a01.chimera.hpc.mssm.edu>
2020-01-02 19:10:01 -0500

    polyfun annotation enrichment plots

--- echolocatoR/R/Finemapping/POLYFUN.R
+++ echolocatoR/R/Finemapping/POLYFUN.R
@@ -739,6 +739,94 @@ POLYFUN.ldsc_annot_enrichment <- function(.results = "Data/GWAS/Nalls23andMe_201
 }
 
 
+  
+POLYFUN.gather_annot_proportions <- function(base_url="/sc/orga/projects/pd-omics/tools/polyfun/annotations/baselineLF2.2.UKB"){
+  prop.file <- file.path(base_url,"annot_proportions.csv")
+  if(!file.exists(prop.file)){
+    all_annot <- list.files(base_url, pattern = "*.annot.parquet$", full.names = T) 
+  annot_PROP <- parallel::mclapply(all_annot, function(x){
+    print(x)
+    annot <- pd$read_parquet(path = x) 
+    annot_sums <- colSums(annot[,6:ncol(annot)]) 
+    annot_prop <- annot_sums/nrow(annot)
+    return(annot_prop)
+  }, mc.cores = 4)
+  
+  annot_PROP_DF <- do.call("cbind",annot_PROP) %>% `colnames<-`(paste0("chr",1:length(annot_PROP)))
+  write.csv(annot_PROP_DF, file = prop.file)
+  } else {
+    annot_PROP_DF <- read.csv(prop.file, row.names = 1)
+  } 
+  return(annot_PROP_DF)
+} 
+
+
+POLYFUN.functional_enrichment <- function(finemap_DT,
+                                          PP_thresh=.95,
+                                          save_plot="./Data/GWAS/Nalls23andMe_2019/_genome_wide/PolyFun/annot_enrichment.png"){
+  # "...functional enrichment of fine-mapped common SNPs in the PIP range, 
+  ## defined as the proportion of common SNPs in the PIP range lying in the annotation 
+  ## divided by the proportion of genome-wide common SNPs lying in the annotation" 
+  base_url <- "/sc/orga/projects/pd-omics/tools/polyfun/annotations/baselineLF2.2.UKB"
+  chrom <- finemap_DT$CHR[1]
+  annot.file <- list.files(base_url, pattern = paste0(".UKB.",chrom,".annot.parquet"), full.names = T)
+  
+  library(reticulate)
+  reticulate::use_condaenv(condaenv = "polyfun_venv") 
+  pd <- reticulate::import("pandas")
+  annot <- pd$read_parquet(path = annot.file) 
+  annot_names <- annot %>% dplyr::select(-c(SNP,CHR,BP,A1,A2)) %>% colnames()
+  annot_DT <- data.table:::merge.data.table(finemap_DT, 
+                                            data.table::data.table(annot) %>%
+                                              dplyr::rename(SNP_y = SNP, A1_y=A1, A2_y=A2),
+                                            by.x = c("CHR","POS"),
+                                            by.y = c("CHR","BP"), 
+                                            all.x = T)
+  ## SNP Groups
+  # Nominal sig. GWAS
+  nom.sig.GWAS <- annot_DT %>% dplyr::summarise_at(.vars = vars(annot_names),
+                                          .funs = funs(sum(P < 0.05 & .>0) / n())) %>% t() %>% `colnames<-`("nom.sig.GWAS")  
+  # Sig. GWAS
+  sig.GWAS <- annot_DT %>% dplyr::summarise_at(.vars = vars(annot_names),
+                                                   .funs = funs(sum(P < 5e-8 & .>0) / n())) %>% t() %>% `colnames<-`("sig.GWAS") 
+  # Lead GWAS
+  lead.GWAS <- annot_DT %>% dplyr::summarise_at(.vars = vars(annot_names),
+                                               .funs = funs(sum(leadSNP==T & .>0) / n())) %>% t() %>% `colnames<-`("lead.GWAS") 
+  # Across all CS
+  UCS <- annot_DT %>% dplyr::summarise_at(.vars = vars(annot_names),
+                                   .funs = funs(sum(Support > 0 & .>0) / n())) %>% t() %>% `colnames<-`("UCS")  
+  # Tool-specific CS
+  FM_methods <- gsub("*\\.PP$","",grep(pattern = "*\\.PP$",colnames(finemap_DT), value = T))
+  CS <- lapply(FM_methods, function(m){
+    print(m)
+    annot_mod <- annot_DT
+    # col_ind <- grep(paste0("^",m,".PP$"),colnames(annot_mod))  
+    # colnames(annot_mod)[col_ind] <- "target.PP"
+    annot_mod <- annot_mod %>% dplyr::select(paste0(m,".PP"),"Support",annot_names)
+    colnames(annot_mod)[1] <- "PP" 
+    annot_mod %>% dplyr::summarise_at(.vars = vars(annot_names),
+                                     .funs = funs( sum(PP > PP_thresh & .>0) / n() ))  
+  }) %>% data.table::rbindlist() %>% t() %>% `colnames<-`(FM_methods)
+  
+  # Gather annotation proportions genome-wide
+  annot_SUMS_DF <- POLYFUN.gather_annot_proportions(base_url) 
+  SNP_groups <- cbind(nom.sig.GWAS, sig.GWAS, lead.GWAS, UCS, CS) 
+  enrich <- (SNP_groups / rowMeans(annot_SUMS_DF)) %>% melt() %>% 
+    `colnames<-`(c("Annot","SNP.Group","Enrichment")) 
+  
+  # Plot
+  gp <- ggplot(enrich, aes(x=Annot, y=Enrichment, fill=SNP.Group)) + 
+    geom_col(position="dodge", show.legend = F) + coord_flip() + 
+    facet_grid(facets = . ~ SNP.Group) + #scales = "free_x"
+    theme_bw()+ theme(axis.text.y = element_text(size = 7))
+  print(gp)
+  if(save_plot!=F){
+    ggsave(filename = save_plot, plot = gp, dpi = 400, height = 20, width = 12)
+  }
+  return(enrich)
+}
+
+
 POLYFUN.h2_enrichment <- function(h2_df, 
                                   target_SNPs=NULL){  
   # Only consider SNPs that overlap between LDCS and GWAS results to make things fair 
